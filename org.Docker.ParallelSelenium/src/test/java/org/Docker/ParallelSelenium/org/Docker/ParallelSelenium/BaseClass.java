d70f2a233f1cad8c7665b774d36939435a4f9d92
ramesvar <ramesvarjobsearch@gmail.com>
2022-12-03 23:12:41 +0100

    added files

--- org.Docker.ParallelSelenium/src/test/java/org/Docker/ParallelSelenium/org/Docker/ParallelSelenium/BaseClass.java
+++ org.Docker.ParallelSelenium/src/test/java/org/Docker/ParallelSelenium/org/Docker/ParallelSelenium/BaseClass.java
@@ -2,15 +2,19 @@ package org.Docker.ParallelSelenium.org.Docker.ParallelSelenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.manager.SeleniumManager;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.service.DriverService;
 
 public class BaseClass {
 	
 	public static void main(String args[]) {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setBrowserName("chrome");
-		WebDriver driver = new RemoteWebDriver(capabilities);
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Ramesh\\Selenium\\ChromeDriver\\106.0.5249.61\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		System.out.println(SeleniumManager.getInstance().getDriverPath("chromedriver"));
 		
 	}
 
